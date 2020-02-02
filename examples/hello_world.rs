20b72d02d017655d8b342aad88cf9dacf2abaeaa
Yuriy O'Donnell <yuriyo@gmail.com>
2020-02-02 11:14:22 +0400

    Added rush::App trait

--- examples/hello_world.rs
+++ examples/hello_world.rs
@@ -1,5 +1,33 @@
 extern crate rush;
 
+use rush::App;
+
+struct HelloWorldApp
+{
+	counter : u32,
+}
+
+impl App for HelloWorldApp
+{
+	fn on_startup(&mut self) {
+		println!("HelloWorldApp::startup()");
+	}
+
+	fn on_update(&mut self)	{
+		if self.counter % 25 == 0 {
+			println!("HelloWorldApp::update() {}", self.counter);
+		}
+		self.counter += 1;
+	}
+
+	fn on_shutdown(&mut self) {
+		println!("HelloWorldApp::shutdown()");
+	}
+}
+
 fn main() {
-	rush::run();
+	let app = Box::new(HelloWorldApp{
+		counter: 0
+	});
+	rush::run(app);
 }
--- src/lib.rs
+++ src/lib.rs
@@ -3,28 +3,41 @@ extern crate rush_sys;
 use std::os::raw::c_void;
 use rush_sys::*;
 
-#[no_mangle]
-pub extern "C" fn on_startup(_user_data: *mut c_void) {
-    println!("on_startup (rust)");
+pub trait App {
+    fn on_startup(&mut self){}
+    fn on_update(&mut self){}
+    fn on_shutdown(&mut self){}
 }
 
 #[no_mangle]
-pub extern "C" fn on_update(_user_data: *mut c_void) {
-    println!("on_update (rust)");
+pub extern "C" fn on_startup(user_data: *mut c_void) {
+    let app_box : &mut Box<dyn App> = unsafe {std::mem::transmute(user_data)};
+    app_box.on_startup();
 }
 
 #[no_mangle]
-pub extern "C" fn on_shutdown(_user_data: *mut c_void) {
-    println!("on_shutdown (rust)");
+pub extern "C" fn on_update(user_data: *mut c_void) {
+    let app_box : &mut Box<dyn App> = unsafe {std::mem::transmute(user_data)};
+    app_box.on_update();
 }
 
-pub fn run() -> i32 {
+#[no_mangle]
+pub extern "C" fn on_shutdown(user_data: *mut c_void) {
+    let app_box : &mut Box<dyn App> = unsafe {std::mem::transmute(user_data)};
+    app_box.on_shutdown();
+}
+
+pub fn run(app : Box<dyn App>) -> i32 {
 
     let mut app_cfg = rush_app_config::new();
 
+    let user_data_box : Box<Box<dyn App>> = Box::new(app);
+    app_cfg.user_data = Box::into_raw(user_data_box) as *mut _;
     app_cfg.on_startup = Some(on_startup);
     app_cfg.on_update = Some(on_update);
     app_cfg.on_shutdown = Some(on_shutdown);
 
-    unsafe { rush_sys::rush_platform_main(&app_cfg) }
+    let err_code = unsafe { rush_sys::rush_platform_main(&app_cfg) };
+
+    err_code
 }
